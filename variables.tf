6bf9297e35219d01c039351a444f4f2eebb3de35
Aron Rightious <aron.rightious@graymeta.com>
2021-02-09 06:42:23 -0800

    var descriptions

--- variables.tf
+++ variables.tf
@@ -1,45 +1,56 @@
 variable "base_ami" {
   type    = string
   default = ""
+  description = "The AMI from which to launch the instance."
 }
 
 variable "hostname_prefix" {
   type = string
+  description = "Creates a unique name beginning with the specified prefix."
 }
 
 variable "instance_type" {
   type = string
+  description = "The type of the EC2 instance."
 }
 
 variable "key_name" {
   type = string
+  description = "The key name to use for the instance(s)."
 }
 
 variable "os_disk_type" {
   type    = string
   default = "gp2"
+  description = "EBS volume type."
 }
 variable "os_disk_size" {
   type    = string
   default = "100"
+    description = "EBS volume size."
 }
 
 variable "size_desired" {
   type = string
+  description = "The number of EC2 instances that should be running in the group."
 }
 variable "size_max" {
   type = string
+  description = "Maximum size of the Auto Scaling Group."
 }
 variable "size_min" {
   type = string
+  description = "Minimum size of the Auto Scaling Group."
 }
 
 variable "subnet_id" {
   type = list(string)
+  description = "A list of subnet IDs to launch resources in."
 }
 
 variable "ssl_certificate_arn" {
   type = string
+  description = "The ARN of the default SSL server certificate."
 }
 
 variable "tags" {
