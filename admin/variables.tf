99c760eccb547d2c13ee2dc601f06074c24c1458
Aron Rightious <aron.rightious@graymeta.com>
2021-02-19 12:51:20 -0800

    address pr comments

--- admin/variables.tf
+++ admin/variables.tf
@@ -4,6 +4,11 @@ variable "ami" {
   default     = ""
 }
 
+variable "instance_profile" {
+  type        = string
+  description = " IAM Instance Profile to launch the instance with."
+}
+
 variable "instance_type" {
   type        = string
   description = "The type of the EC2 instance."
@@ -12,6 +17,30 @@ variable "instance_type" {
 variable "key_name" {
   type        = string
   description = "The key name to use for the instance(s)."
+}
+
+variable "security_groups" {
+  type        = string
+  description = "A list of security groups to associate with."
+}
+
+variable "subnet_id" {
+  type        = string
+  description = "VPC Subnet ID to launch in."
+}
+
+variable "tags" {
+  type        = map(string)
+  description = "A map of the tags to use on the resources that are deployed with this module. (Default: source = terraform)"
+
+  default = {
+    source = "terraform"
+  }
+}
+
+variable "user_data" {
+  type        = string
+  description = "User data to provide when launching the instance."
 }
 
 variable "volume_type" {
