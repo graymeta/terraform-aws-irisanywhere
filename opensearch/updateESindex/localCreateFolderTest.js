a4b583076293f817c7cf0c474e8d28f80463f65e
Scott Sharp <scott@reachconsulting.io>
2021-11-15 15:28:14 -0800

    removed access key

--- opensearch/updateESindex/localCreateFolderTest.js
+++ opensearch/updateESindex/localCreateFolderTest.js
@@ -7,8 +7,6 @@ process.env.AWS_REGION = 'us-west-2';
 process.env.language = 'en';
 process.env.domain = 'os-endpoint.graymeta.com';
 process.env.region = 'us-west-2';
-process.env.domain_key_id = 'AKIA5JKV74OUZBRCT2MD';
-process.env.domain_secret_key = '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';
 
 // Lambda handler
 const { handler } = require('./index')
