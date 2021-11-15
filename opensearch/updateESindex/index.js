e10c573c387f1266d89179c0808e9ca53c2a24ae
Scott Sharp <scott@reachconsulting.io>
2021-11-15 00:36:31 -0800

    Added proper cloudwatch logging messages

--- opensearch/updateESindex/index.js
+++ opensearch/updateESindex/index.js
@@ -11,104 +11,105 @@
  *
  **************************/
 
-'use strict'
+ 'use strict'
 
-const AWS = require('aws-sdk');
-
-var opensearch = new AWS.OpenSearch({ endpoint : process.env.domain });
-
-AWS.config.region = process.env.AWS_REGION 
-
-// The Lambda handler
-exports.handler = async (event) => {
-
-  // Handle each incoming S3 object in the event
-  await Promise.all(
-    event.Records.map(async (event) => {
-      //console.log(event);
-      try {
-        if (event.eventName.startsWith('ObjectCreated:')) {
-          await processCreateEvent(event)
-        } else if (event.eventName.startsWith('ObjectRemoved:')) {
-          await processDeleteEvent(event)
-        }
-      } catch (err) {
-        console.error(`Handler error: ${err}`)
-      }
-    })
-  )
-}
-
-// Add S3 Object to bucket index
-const processCreateEvent = async (event) => {
-  
-  // Payload object for ES index insertion
-  var response = await openSearchClient('POST', event.s3.bucket.name + '/_doc', JSON.stringify({
-    s3key : event.s3.object.key,
-    filepath: event.s3.object.key,
-    filename: event.s3.object.key.replace(/^.*[\\\/]/, ''),
-    bucket: event.s3.bucket.name,
-    etag: event.s3.object.eTag,
-    filesize: event.s3.object.Size,
-    lastmodified : Date.now()
-  }));
-}
-
-// Delete S3 Object from bucket index
-const processDeleteEvent = async (event) => {
-  
-  var requestBody = {"query": {"term": {"s3key": event.s3.object.key}}}
-  openSearchClient('GET', event.s3.bucket.name + '/_search', JSON.stringify(requestBody)).then((searchResponse) => {
-    var deletedEntry = false;
-    //console.log(searchResponse);
-    searchResponse.hits.hits.map(async (searchHit) => {
-
-        // Objects in S3 are globally unique when inclueding the bucket with the path
-        if (searchHit._source.bucket.trim() == event.s3.bucket.name.trim()) {
-          if (searchHit._source.filepath.trim() == event.s3.object.key.trim()) {
-            //console.log(searchHit);
-            deletedEntry = true;
-            var response = openSearchClient('DELETE', event.s3.bucket.name + '/_doc/' + searchHit._id, '');
-          }
-        }
-    });
-    if (!deletedEntry) {
-      console.log("Didn't delete cache item for event!\n\n" + JSON.stringify(event));
-    }
-  });
-}
-
-const openSearchClient = async (httpMethod, path, requestBody) => {
-  return new Promise((resolve, reject) => {
-    const endpoint = new AWS.Endpoint(process.env.domain)
-    let request = new AWS.HttpRequest(endpoint, process.env.AWS_REGION)
-
-    //console.log(requestBody);
-
-    request.method = httpMethod;
-    request.path += path;
-    request.body = requestBody;
-    request.headers['host'] = endpoint.host;
-    request.headers['Content-Type'] = 'application/json';
-    request.headers['Content-Length'] = Buffer.byteLength(request.body)
-
-    const credentials = new AWS.SharedIniFileCredentials('default')
-    const signer = new AWS.Signers.V4(request, 'es')
-    signer.addAuthorization(credentials, new Date())
-
-    const client = new AWS.HttpClient()
-    client.handleRequest(request, null, function(response) {
-      //console.log(response.statusCode + ' ' + response.statusMessage)
-      let responseBody = ''
-      response.on('data', function (chunk) {
-        responseBody += chunk;
-      });
-      response.on('end', function (chunk) {
-        resolve(JSON.parse(responseBody));
-      });
-    }, function(error) {
-      console.log('Error: ' + error)
-      reject()
-    })
-  })
-}
+ const AWS = require('aws-sdk');
+ 
+ AWS.config.region = process.env.region 
+ 
+ // The Lambda handler
+ exports.handler = (event) => {
+ 
+   // Handle each incoming S3 object in the event
+   Promise.all(
+     event.Records.map((event) => {
+       
+       try {
+         if (event.eventName.startsWith('ObjectCreated:')) {
+           processCreateEvent(event)
+         } else if (event.eventName.startsWith('ObjectRemoved:')) {
+           processDeleteEvent(event)
+         }
+       } catch (err) {
+         console.error(`Handler error: ${err}`)
+       }
+     })
+   )
+ }
+ 
+ // Add S3 Object to bucket index
+ const processCreateEvent = (event) => {
+   //console.log("Created:" +  event);
+   // Payload object for ES index insertion
+   var response = openSearchClient('POST', event.s3.bucket.name + '/_doc', JSON.stringify({
+     s3key : event.s3.object.key,
+     filepath: event.s3.object.key,
+     filename: event.s3.object.key.replace(/^.*[\\\/]/, ''),
+     bucket: event.s3.bucket.name,
+     etag: event.s3.object.eTag,
+     filesize: event.s3.object.Size,
+     lastmodified : Date.now()
+   }));
+   console.log("Inserted index item for event!\n\n" + JSON.stringify(event));
+ }
+ 
+ // Delete S3 Object from bucket index
+ const processDeleteEvent = (event) => {
+   
+   var requestBody = {"query": {"term": {"s3key": event.s3.object.key}}}
+   openSearchClient('GET', event.s3.bucket.name + '/_search', JSON.stringify(requestBody)).then((searchResponse) => {
+     var deletedEntry = false;
+     //console.log(searchResponse);
+     searchResponse.hits.hits.map((searchHit) => {
+ 
+         // Objects in S3 are globally unique when inclueding the bucket with the path
+         if (searchHit._source.bucket.trim() == event.s3.bucket.name.trim()) {
+           if (searchHit._source.filepath.trim() == event.s3.object.key.trim()) {
+             //console.log(searchHit);
+             deletedEntry = true;
+             var response = openSearchClient('DELETE', event.s3.bucket.name + '/_doc/' + searchHit._id, '');
+             console.log("Deleted index item for event!\n\n" + JSON.stringify(event));
+           }
+         }
+     });
+     if (!deletedEntry) {
+       console.log("Didn't delete index item for event!\n\n" + JSON.stringify(event));
+     }
+   });
+ }
+ 
+ const openSearchClient = (httpMethod, path, requestBody) => {
+   return new Promise((resolve, reject) => {
+     const endpoint = new AWS.Endpoint(process.env.domain)
+     let request = new AWS.HttpRequest(endpoint, process.env.AWS_REGION)
+ 
+     //console.log(requestBody);
+ 
+     request.method = httpMethod;
+     request.path += path;
+     request.body = requestBody;
+     request.headers['host'] = endpoint.host;
+     request.headers['Content-Type'] = 'application/json';
+     request.headers['Content-Length'] = Buffer.byteLength(request.body)
+ 
+     const credentials = { accessKeyId: process.env.key_id, secretAccessKey: process.env.secret_key, region: process.env.region }
+     const signer = new AWS.Signers.V4(request, 'es')
+     signer.addAuthorization(credentials, new Date())
+ 
+     const client = new AWS.HttpClient()
+     client.handleRequest(request, null, function(response) {
+       //console.log(response.statusCode + ' ' + response.statusMessage)
+       let responseBody = ''
+       response.on('data', function (chunk) {
+         responseBody += chunk;
+       });
+       response.on('end', function (chunk) {
+         //console.log(responseBody);
+         resolve(JSON.parse(responseBody));
+       });
+     }, function(error) {
+       console.log('Error: ' + error)
+       reject()
+     })
+   })
+ }
