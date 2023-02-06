0cd6f0661b10bfb14379e540c0bf22f6d0bdd202
Allermann, Chris (206424915) <Chris.Allermann@nbcuni.com>
2023-02-06 11:40:58 -0500

    Add instance_initiated_shutdown_behavior to ASG launch template

--- asg/asg.tf
+++ asg/asg.tf
@@ -88,6 +88,7 @@ resource "aws_launch_template" "iris" {
   key_name      = var.key_name
   user_data     = base64encode(join("\n", ["<powershell>", "${data.template_file.cloud_init.rendered}", "${var.user_init}", "\n", "Restart-Computer -Force", "\n", "</powershell>"]))
   ebs_optimized = true
+  instance_initiated_shutdown_behavior = var.terminate_on_shutdown ? "terminate" : "stop"
 
   iam_instance_profile {
     name = aws_iam_instance_profile.iris.name
