3753c95eb64b37405d2b376f6f6248724fa00c8c
Thomas Marble <thomas.marble@graymeta.com>
2024-04-05 11:55:32 -0600

    500

--- asg/asg.tf
+++ asg/asg.tf
@@ -54,7 +54,7 @@ resource "aws_autoscaling_lifecycle_hook" "iris_init" {
   name                   = "iris_init"
   autoscaling_group_name = aws_autoscaling_group.iris.name
   default_result         = "CONTINUE"
-  heartbeat_timeout      = 400
+  heartbeat_timeout      = 500
   lifecycle_transition   = "autoscaling:EC2_INSTANCE_LAUNCHING"
 }
 
@@ -152,7 +152,7 @@ resource "aws_launch_template" "iris" {
 
 resource "aws_autoscaling_policy" "out" {
   name                   = replace("${var.hostname_prefix}-${var.instance_type}-ScaleOut", ".", "")
-  scaling_adjustment     = 2
+  scaling_adjustment     = var.asg_scaleout_adjustment
   adjustment_type        = "ChangeInCapacity"
   cooldown               = var.asg_scaleout_cooldown
   autoscaling_group_name = aws_autoscaling_group.iris.name
