2a5f5a0c9fda27fb6eb4c4821459288905b972df
Jon Flaig <71786727+GM-JFlaig@users.noreply.github.com>
2025-01-09 13:17:34 -0600

    Merge all new rclone code along with secrets variable defaults (#142)
    * Final merge of lambda-enhancements and ha-name-variable
    
    * replaced archived data template_file {} with templatefile() function. This allows terraform darwin/arm binary to now work
    
    * Changed output local_policy_text to be compatible with new template code
    
    * more rclone optimizations
    
    * Modify cloud_init.tpl to match the new master version
    
    * Latest v2.0.0 code that includes more defaults in secrets variables and more rclone optimizations

--- secrets/variables.tf
+++ secrets/variables.tf
@@ -54,18 +54,21 @@ variable "admin_server" {
 variable "iris_s3_bucketname" {
   type        = string
   description = "Name of S3 Bucket"
+  default     = ""
 }
 
 variable "iris_s3_access_key" {
   type        = string
   description = "S3 access key cred for Iris"
   sensitive   = true
+  default     = ""
 }
 
 variable "iris_s3_secret_key" {
   type        = string
   description = "S3 secret key cred for Iris"
   sensitive   = true
+  default     = ""
 }
 
 variable "iris_s3_lic_code" {
@@ -90,60 +93,71 @@ variable "iris_serviceacct" {
 variable "okta_issuer" {
   type        = string
   description = "Value for Okta Issuer SPA"
+  default     = ""
 }
 
 variable "okta_clientid" {
   type        = string
   description = "Value for Okta Client ID SPA"
+  default     = ""
 }
 
 variable "okta_redirecturi" {
   type        = string
   description = "Value for Okta callback URI SPA"
+  default     = ""
 }
 
 variable "okta_scope" {
   type        = string
   description = "Value for scope SPA"
+  default     = ""
 }
 
 variable "s3_meta_access_key" {
   type        = string
   description = "S3 Access key for alternate bucket write access"
   sensitive   = true
+  default     = ""
 }
 
 variable "s3_meta_secret_key" {
   type        = string
   description = "S3 Secret key for alternate bucket write access"
   sensitive   = true
+  default     = ""
 }
 
 variable "s3_meta_bucketname" {
   type        = string
   description = "S3 bucket name for alternate bucket write access"
+  default     = ""
 }
 
 variable "os_region" {
   type        = string
   description = "Region OpenSearch is deployed"
+  default     = ""
 }
 
 variable "os_endpoint" {
   type        = string
   description = "FQDN of OpenSearch endpoint"
+  default     = ""
 }
 
 variable "os_accessid" {
   type        = string
   description = "AccessID for signed OpenSearch processes"
   sensitive   = true
+  default     = ""
 }
 
 variable "os_secretkey" {
   type        = string
   description = "SecretKey for signed OpenSearch processes"
   sensitive   = true
+  default     = ""
 }
 
 variable "s3_enterprise" {
@@ -154,32 +168,38 @@ variable "s3_enterprise" {
 variable "saml_uniqueID" {
   type        = string
   description = "(Optional) ID of IDP for SAML configuration (Either okta or ping-identity)."
+  default     = ""
 }
 
 variable "saml_displayName" {
 
   type        = string
   description = "(Optional) Display name of IDP for SAML configuration (Either Okta or Ping Identity)."
+  default     = ""
 }
 
 variable "saml_entryPoint" {
   type        = string
   description = "(Optional) Identity Provider Single Sign-On URL."
+  default     = ""
 }
 
 variable "saml_samlissuer" {
   type        = string
   description = "(Optional) - Identity Provider Issuer from SAML configuration (within SAML setup)."
+  default     = ""
 }
 
 variable "saml_acsUrlBasePath" {
   type        = string
   description = "(Optional) - URL path to visit Iris Anywhere (iris-url.domain.com/irisanywhere)."
+  default     = ""
 }
 
 variable "saml_acsUrlRelativePath" {
   type        = string
   description = "(Optional) ACS URL relative to Iris Anywhere server (eg. /auth/saml/idp/assertion-consumer-service)"
+  default     = ""
 }
 
 variable "tags" {
