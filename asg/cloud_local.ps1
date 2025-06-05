2222edefb577d28bee7eb556d35f335e6894df27
Jon Flaig <jon.flaig@graymeta.com>
2025-06-05 15:42:29 -0500

    Added new get-metadata function to get IMDSv2 info

--- asg/cloud_local.ps1
+++ asg/cloud_local.ps1
@@ -78,24 +78,16 @@ else {
 Set-Service -Name AmazonSSMAgent -StartupType Automatic ; Start-Service AmazonSSMAgent
 
 #Update Instance
-    # $webclient = new-object net.webclient
-    # $instanceid = $webclient.Downloadstring('http://169.254.169.254/latest/meta-data/instance-id')
-    # [System.Environment]::SetEnvironmentVariable('INSTANCE_ID', $instanceid, [System.EnvironmentVariableTarget]::Machine)
-    # [System.Environment]::SetEnvironmentVariable('INSTANCE_NAME', "${name}-"+$instanceid, [System.EnvironmentVariableTarget]::Machine)
-    # Import-Module -name AWSPowerShell
-    # $tag = New-Object Amazon.EC2.Model.Tag
-    # $tag.Key = "Name"
-    # $tag.Value = "${name}-"+$instanceid
-    # New-EC2Tag -Resource $instanceid -Tag $tag
+    # # Step 1: Request a token from IMDSv2
+    # $tokenUri = "http://169.254.169.254/latest/api/token"
+    # $headers = @{ "X-aws-ec2-metadata-token-ttl-seconds" = "120" }  #2 mins
+    # $token = Invoke-RestMethod -Method Put -Uri $tokenUri -Headers $headers
 
-    # Step 1: Request a token from IMDSv2
-    $tokenUri = "http://169.254.169.254/latest/api/token"
-    $headers = @{ "X-aws-ec2-metadata-token-ttl-seconds" = "120" }  #2 mins
-    $token = Invoke-RestMethod -Method Put -Uri $tokenUri -Headers $headers
+    # # Step 2: Use the token to access instance metadata
+    # $metadataUri = "http://169.254.169.254/latest/meta-data/instance-id"
+    # $instanceid = Invoke-RestMethod -Uri $metadataUri -Headers @{ "X-aws-ec2-metadata-token" = $token }
 
-    # Step 2: Use the token to access instance metadata
-    $metadataUri = "http://169.254.169.254/latest/meta-data/instance-id"
-    $instanceid = Invoke-RestMethod -Uri $metadataUri -Headers @{ "X-aws-ec2-metadata-token" = $token }
+    $instanceid = Get-MetaData -MetaDataType "instanceId"
 
     # Step 3: Set environment variables
     [System.Environment]::SetEnvironmentVariable('INSTANCE_ID', $instanceid, [System.EnvironmentVariableTarget]::Machine)
@@ -108,6 +100,5 @@ Set-Service -Name AmazonSSMAgent -StartupType Automatic ; Start-Service AmazonSS
     $tag.Value = "${name}-$instanceid"
     New-EC2Tag -Resource $instanceid -Tag $tag
     
-
     Write-EventLog -LogName IrisAnywhere -source IrisAnywhere -EntryType Information -eventid 1000 -message "Init Complete - Restarting"
     Rename-Computer -NewName $instanceid -force
