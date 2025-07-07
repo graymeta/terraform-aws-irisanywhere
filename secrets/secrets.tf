5f2afa7396e93a3bcbb7d5b4d0fd62284ca95d08
Thomas Marble <thomas.marble@graymeta.com>
2025-07-07 12:41:37 -0600

    defualts

--- secrets/secrets.tf
+++ secrets/secrets.tf
@@ -9,20 +9,19 @@ resource "aws_secretsmanager_secret" "iris_config" {
 resource "aws_secretsmanager_secret_version" "iris_config" {
   secret_id     = aws_secretsmanager_secret.iris_config.id
   secret_string = <<EOF
-  {
+{
   "admin_console_id":   "${var.admin_console_id}",
   "admin_console_pw":   "${var.admin_console_pw}",
   "admin_db_id":        "${var.admin_db_id}",
   "admin_db_pw":        "${var.admin_db_pw}",
   "admin_server":       "",
   "admin_customer_id":  "",
-  "s3_enterprise":      s3_enterprise = jsonencode(<<EOT
-		{ "buckets": [
-			{"name": "name1","enabled": true},
-			{"name": "name2","enabled": true}
-		]}
-		EOT
-	)
+  "s3_enterprise": {
+    "buckets": [
+      { "name": "name1", "enabled": true },
+      { "name": "name2", "enabled": true }
+    ]
+  }
 }
 EOF
 }
