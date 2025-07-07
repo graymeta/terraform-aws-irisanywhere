9ea3157f13004ebc76709a769bd58c4c13180a7d
Thomas Marble <thomas.marble@graymeta.com>
2025-07-07 12:58:10 -0600

    defaults2

--- secrets/secrets.tf
+++ secrets/secrets.tf
@@ -17,11 +17,15 @@ resource "aws_secretsmanager_secret_version" "iris_config" {
     admin_db_pw        = var.admin_db_pw,
     admin_server       = "",
     admin_customer_id  = "",
-    s3_enterprise = {
-      buckets = [
-        { name = "name1", enabled = true },
-        { name = "name2", enabled = true }
-      ]
-    }
+
+    s3_enterprise = jsondecode(<<EOT
+{
+  "buckets": [
+    { "name": "name1", "enabled": true },
+    { "name": "name2", "enabled": true }
+  ]
+}
+EOT
+    )
   })
 }
