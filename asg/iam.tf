976f0f8ecfb3e4496fc0d8b0976b8fbce78bdee6
Jon Flaig <jon.flaig@graymeta.com>
2025-07-15 14:23:54 -0500

    Remove unnecessary comments

--- asg/iam.tf
+++ asg/iam.tf
@@ -1,23 +1,5 @@
-# data "template_file" "iris_role" {
-#   template = file("${path.module}/role.json")
-# }
-
-# data "template_file" "iris_policy_base" {
-#   template = file("${path.module}/policy.json")
-
-#   vars = {
-#     cluster = replace("${var.hostname_prefix}-${var.deployment_name != "1" ? var.deployment_name : var.instance_type}", ".", "")
-#   }
-# }
-
-# data "template_file" "iris_policy_custom" {
-#   #template =  var.s3_policy
-#   template = var.iam_policy_enabled == true ? var.s3_policy : "{}"
-# }
-
 resource "aws_iam_role" "iris" {
   name = var.iam_role_name != "" ? var.iam_role_name : replace("${var.hostname_prefix}-${var.deployment_name != "1" ? var.deployment_name : var.instance_type}-Role", ".", "")
-  #name               = replace("${var.hostname_prefix}-${var.deployment_name != "1" ? var.deployment_name : var.instance_type}-Role", ".", "")
   assume_role_policy = templatefile("${path.module}/role.json",{})
 }
 
