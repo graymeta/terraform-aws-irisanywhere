0a9357f556aa708515fb26b2f07a40a0255d5d96
Jon Flaig <jon.flaig@graymeta.com>
2026-01-17 22:54:07 -0600

    Removed unnecessary vars

--- asg/cloud_local.ps1
+++ asg/cloud_local.ps1
@@ -7,14 +7,14 @@ Write-EventLog -LogName IrisAnywhere -source IrisAnywhere -EntryType Information
     $iasecretarn = "${ia_secret_arn}"
     $iadomain = "${ia_domain}"
     $search_enabled = "${search_enabled}"
-    $ia_video_bitrate = "${ia_video_bitrate}"
-    $ia_video_codec = "${ia_video_codec}"
+    #$ia_video_bitrate = "${ia_video_bitrate}"
+    #$ia_video_codec = "${ia_video_codec}"
     $s3_enterprise = "${s3_enterprise}"
     $haproxy = "${haproxy}"
     $saml_enabled = "${saml_enabled}"
     $saml_cert_secret_arn = "${saml_cert_secret_arn}"
     $disk_data_size = "${disk_data_size}"
-    $wasabi = "${wasabi}"
+    #$wasabi = "${wasabi}"
     #Retrieve and prepare Secrets
     try {
         $secretdata = get-SECsecretValue $iasecretarn ; $secretdata=$secretdata.secretstring | convertfrom-json
@@ -35,35 +35,38 @@ Write-EventLog -LogName IrisAnywhere -source IrisAnywhere -EntryType Information
         $saml_samlissuer        = $secretdata.saml_samlissuer
         $saml_acsUrlBasePath    = $secretdata.saml_acsUrlBasePath
         $saml_acsUrlRelativePath= $secretdata.saml_acsUrlRelativePath
-        $wasabi_access_key       = $secretdata.wasabi_access_key
-        $wasabi_secret_access_key= $secretdata.wasabi_secret_access_key
-        $wasabi_endpoint         = $secretdata.wasabi_endpoint
-        $wasabi_region            = $secretdata.wasabi_region
-        $wasabi_buckets          = $secretdata.wasabi_buckets
+        #$wasabi_access_key       = $secretdata.wasabi_access_key
+        #$wasabi_secret_access_key= $secretdata.wasabi_secret_access_key
+        #$wasabi_endpoint         = $secretdata.wasabi_endpoint
+        #$wasabi_region            = $secretdata.wasabi_region
+        #$wasabi_buckets          = $secretdata.wasabi_buckets
     }
     catch {
         Write-EventLog -LogName IrisAnywhere -source IrisAnywhere -EntryType Error -eventid 1001 -message "Exception accessing secret $iasecretarn"
     }
 
-#Run init locally
-Write-EventLog -LogName IrisAnywhere -source IrisAnywhere -EntryType Information -eventid 1000 -message "cache_content is: $cache_content"
 
-& "C:\ProgramData\GrayMeta\launch\scripts\local_init_enterprise_rclone.ps1"
+    try {
+        & "C:\ProgramData\GrayMeta\launch\scripts\local_init_enterprise_rclone.ps1"
+    } catch {
+        Write-EventLog -LogName IrisAnywhere -source IrisAnywhere -EntryType `
+        Error -eventid 1002 -message "Exception executing local_init_enterprise_rclone.ps1: $_"
+    }
 
 #Start SSM Service
 Set-Service -Name AmazonSSMAgent -StartupType Automatic ; Start-Service AmazonSSMAgent
 
 #Update Instance
-    $instanceid = Get-MetaData -MetaDataType "instanceId"
+$instanceid = Get-MetaData -MetaDataType "instanceId"
 
-    [System.Environment]::SetEnvironmentVariable('INSTANCE_ID', $instanceid, [System.EnvironmentVariableTarget]::Machine)
-    [System.Environment]::SetEnvironmentVariable('INSTANCE_NAME', "${name}-$instanceid", [System.EnvironmentVariableTarget]::Machine)
+[System.Environment]::SetEnvironmentVariable('INSTANCE_ID', $instanceid, [System.EnvironmentVariableTarget]::Machine)
+[System.Environment]::SetEnvironmentVariable('INSTANCE_NAME', "${name}-$instanceid", [System.EnvironmentVariableTarget]::Machine)
 
-    Import-Module -Name AWSPowerShell
-    $tag = New-Object Amazon.EC2.Model.Tag
-    $tag.Key = "Name"
-    $tag.Value = "${name}-$instanceid"
-    New-EC2Tag -Resource $instanceid -Tag $tag
-    
-    Write-EventLog -LogName IrisAnywhere -source IrisAnywhere -EntryType Information -eventid 1000 -message "Init Complete - Restarting"
-    Rename-Computer -NewName $instanceid -force
+Import-Module -Name AWSPowerShell
+$tag = New-Object Amazon.EC2.Model.Tag
+$tag.Key = "Name"
+$tag.Value = "${name}-$instanceid"
+New-EC2Tag -Resource $instanceid -Tag $tag
+
+Write-EventLog -LogName IrisAnywhere -source IrisAnywhere -EntryType Information -eventid 1000 -message "Init Complete - Restarting"
+Rename-Computer -NewName $instanceid -force
