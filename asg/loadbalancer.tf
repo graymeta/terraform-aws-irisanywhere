3b690327b3a4f334ea485e5049041649688ce1a0
Thomas Marble <thomas.marble@graymeta.com>
2026-07-22 10:55:25 -0600

    Switch to lb_cookie stickiness, set duration to 1 hour

--- asg/loadbalancer.tf
+++ asg/loadbalancer.tf
@@ -81,8 +81,7 @@ resource "aws_lb_target_group" "port443" {
   }
 
   stickiness {
-    type            = "app_cookie"
-    cookie_name     = "iris-anywhere-lb-sid"
+    type            = "lb_cookie"
     cookie_duration = var.alb_cookie_duration
     enabled         = true
   }
