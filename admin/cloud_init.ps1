35a64cbc29f36893f79d1972c687368ef002d3de
Jon Flaig <jon.flaig@graymeta.com>
2026-07-29 00:49:57 -0500

    Invoke set-serverid.ps1 in order to generate serverID tag

--- admin/cloud_init.ps1
+++ admin/cloud_init.ps1
@@ -8,7 +8,8 @@ $http_console_port = "${http_console_port}"
 
 #Retrieve and prepare Secrets
 try {
-    $secretdata = get-SECsecretValue $iasecretarn ; $secretdata=$secretdata.secretstring | convertfrom-json
+    $secretdata = Get-SECSecretValue -SecretId $iasecretarn
+    $secretdata = $secretdata.SecretString | ConvertFrom-Json
     #Set init variables
     $admin_db_id        = $secretdata.admin_db_id
     $admin_db_pw        = $secretdata.admin_db_pw
@@ -18,13 +19,14 @@ try {
     #TEMP create event log for debugging
     if (-not (Get-EventLog -List -ErrorAction SilentlyContinue | Where-Object {$_.Log -eq 'IrisAdmin'})) {
         New-EventLog -LogName IrisAdmin -Source "IrisAdmin"
+        Start-Sleep -Seconds 5
     }
-    Start-Sleep -Seconds 10    Write-EventLog -LogName IrisAdmin -source IrisAdmin -EntryType Information -eventid 1001 -message "I have started initializing"
-    }
+    Write-EventLog -LogName IrisAdmin -Source IrisAdmin -EntryType Information -EventId 1001 -Message "Iris Admin has started initializing"
+}
 catch {
     Write-host $_.Exception | Format-List -force
     Write-host "Exception accessing secret $iasecretarn" -ForegroundColor Red 
-    Write-EventLog -LogName IrisAdmin -source IrisAdmin -EntryType Error -eventid 1001 -message "Exception accessing secret $iasecretarn"
+    Write-EventLog -LogName IrisAdmin -Source IrisAdmin -EntryType Error -EventId 1001 -Message "Exception accessing secret $iasecretarn"
 }
 
 $start_time=$(((get-date).ToUniversalTime()).ToString("yyyyMMddTHHmmssZ"))
@@ -34,19 +36,18 @@ $iris_admin_exe = gci "$($env:systemdrive)\IrisTemp\" | where {$_.name -like "*.
 
 Write-Host "Message: Installing IrisAdmin $iadbversion"  -ForegroundColor Green
 try {
-
-if($enterprise_ha -eq "true"){
+    if($enterprise_ha -eq "true") {
+            Start-Process -FilePath "C:\IrisTemp\$($iris_admin_exe)" -ArgumentList  "/S /DBHOST=$dbserver /SERVERPORTHTTPS=$https_console_port /SERVERPORTHTTP=$http_console_port /DATAFOLDER=C:\PostgreSQLData /DBUSERNAME=$admin_db_id /DBPORT=5432 /DBPASSWORD=$admin_db_pw /ADMINUSERNAME=$admin_console_id /ADMINPASSWORD=$admin_console_pw" -Wait -PassThru
+    } else {
+            Start-Process -FilePath "C:\IrisTemp\$($iris_admin_exe)" -ArgumentList  "/S /SERVERPORTHTTPS=$https_console_port /SERVERPORTHTTP=$http_console_port /DATAFOLDER=C:\PostgreSQLData /DBUSERNAME=$admin_db_id /DBPORT=5432 /DBPASSWORD=$admin_db_pw /ADMINUSERNAME=$admin_console_id /ADMINPASSWORD=$admin_console_pw" -Wait -PassThru
+    }
     
-    Start-Process -FilePath "C:\IrisTemp\$($iris_admin_exe)" -ArgumentList  "/S /DBHOST=$dbserver /SERVERPORTHTTPS=$https_console_port /SERVERPORTHTTP=$http_console_port /DATAFOLDER=C:\PostgreSQLData /DBUSERNAME=$admin_db_id /DBPORT=5432 /DBPASSWORD=$admin_db_pw /ADMINUSERNAME=$admin_console_id /ADMINPASSWORD=$admin_console_pw" -Wait -PassThru
-}else{
-    Start-Process -FilePath "C:\IrisTemp\$($iris_admin_exe)" -ArgumentList  "/S /SERVERPORTHTTPS=$https_console_port /SERVERPORTHTTP=$http_console_port /DATAFOLDER=C:\PostgreSQLData /DBUSERNAME=$admin_db_id /DBPORT=5432 /DBPASSWORD=$admin_db_pw /ADMINUSERNAME=$admin_console_id /ADMINPASSWORD=$admin_console_pw" -Wait -PassThru
-}
-    Write-EventLog -LogName IrisAdmin -source IrisAdmin -EntryType Information -eventid 1000 -message "Iris Admin installed"
+    Write-EventLog -LogName IrisAdmin -Source IrisAdmin -EntryType Information -EventId 1000 -Message "Iris Admin installed"
 }
 catch {
     Write-host $_.Exception | Format-List -force
     Write-host "Exception during install process."   
-    Write-EventLog -LogName IrisAdmin -source IrisAdmin -EntryType Error -eventid 1001 -message "Iris Admin failed to install"
+    Write-EventLog -LogName IrisAdmin -Source IrisAdmin -EntryType Error -EventId 1001 -Message "Iris Admin failed to install"
 }
 
 #Check and Cleanup
@@ -54,7 +55,9 @@ if (Test-Path "HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall\GrayMet
     $irisdbvercheck = Get-ItemProperty -Path "HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall\GrayMeta Iris DB Server" -name Displayversion | select -ExpandProperty displayversion
 }
 
-if($irisdbvercheck){
+if($irisdbvercheck) {
+    ## Add the ServerID tag to the instance
+    & "C:\ProgramData\Graymeta\Launch\scripts\set-serverid.ps1"
     Write-host "GrayMeta Iris Server version $irisdbvercheck installed"  -ForegroundColor Green
 } else {
     Write-host "GrayMeta Iris Server installation $irisdbvercheck failed"  -ForegroundColor red
